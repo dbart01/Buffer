1367a88086dc5f2908be779b48ef5a2d0628074f
Dima Bart <dima.bart01@gmail.com>
2018-04-25 06:51:06 -0400

    Initial Commit

---
+++ Buffer/Buffer.h
@@ -0,0 +1,19 @@
+//
+//  Buffer.h
+//  Buffer
+//
+//  Created by Dima Bart on 2018-04-25.
+//  Copyright © 2018 Dima Bart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Buffer.
+FOUNDATION_EXPORT double BufferVersionNumber;
+
+//! Project version string for Buffer.
+FOUNDATION_EXPORT const unsigned char BufferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Buffer/PublicHeader.h>
+
+
